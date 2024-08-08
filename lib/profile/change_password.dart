729e8eaac4472b6855bb50755e89e3067487e97e
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-08-09 00:14:33 +0600

    learners

--- lib/profile/change_password.dart
+++ lib/profile/change_password.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:learners/profile/profile_provider.dart';
+import 'package:provider/provider.dart';
 
 class ChangePassword extends StatefulWidget {
   @override
@@ -18,69 +19,64 @@ class _ChangePasswordState extends State<ChangePassword> {
   late TextEditingController _confirmPasswordController;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool isOldPasswordCorrect = true;
-  int counter = 0;
-  String? _password;
   String? _email;
 
   @override
   void initState() {
     super.initState();
-    _loadEmail();
+    final profileProvider = Provider.of<ProfileProvider>(context, listen: false);
+    _email = profileProvider.email;
     _oldPasswordController = TextEditingController();
     _newPasswordController = TextEditingController();
     _confirmPasswordController = TextEditingController();
   }
-  Future<void> _loadEmail() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? email = prefs.getString('email');
-    if (email != null) {
-      _email = email;
-    }
-  }
 
-  Future<void> _UserDbUpdate(String _password,String _email) async {
-    final String apiUrl = 'http://104.247.108.10:3000/api/user/update/$_email';
+  Future<void> updatePassword(String email, String currentPassword, String newPassword) async {
+    const String uri = "http://192.168.1.13/learners_api/change_password.php";
 
-    final Map<String, String> data = {
-      "password": _password,
+    final body = {
+      'email': email,
+      'current_password': currentPassword,
+      'new_password': newPassword,
     };
 
     final response = await http.post(
-      Uri.parse(apiUrl),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(data),
+      Uri.parse(uri),
+      body: body,
     );
 
     if (response.statusCode == 200) {
-      print('Update successful!');
-      Fluttertoast.showToast(msg: "Profile Updated", gravity: ToastGravity.TOP);
-      print(response.body);
+      final Map<String, dynamic> jsonResponse = json.decode(response.body);
+
+      if (jsonResponse['success'] == 'true') {
+        Navigator.pop(context);
+        Fluttertoast.showToast(msg: jsonResponse['message']);
+      } else {
+        Fluttertoast.showToast(msg: jsonResponse['message']);
+      }
     } else {
-      Fluttertoast.showToast(msg: "Error updating profile", gravity: ToastGravity.TOP);
-      print('Update failed. Status code: ${response.statusCode}');
-      print(response.body);
+      Fluttertoast.showToast(msg: 'Server error: ${response.statusCode}');
     }
   }
+
 
   void _togglePasswordVisibility1() {
     setState(() {
       _passEnable1 = !_passEnable1;
     });
   }
+
   void _togglePasswordVisibility2() {
     setState(() {
       _passEnable2 = !_passEnable2;
     });
   }
+
   void _togglePasswordVisibility3() {
     setState(() {
       _passEnable3 = !_passEnable3;
     });
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -91,15 +87,17 @@ class _ChangePasswordState extends State<ChangePassword> {
         backgroundColor: Colors.orange,
       ),
 
-
       body: SingleChildScrollView(
-        child: Padding(
+        child: Container(
           padding: const EdgeInsets.all(16.0),
           child: Form(
             key: _formKey,
+
             child: Column(
               children: [
+
                 SizedBox(height: 20),
+
                 TextFormField(
                   controller: _oldPasswordController,
                   obscureText: _passEnable1,
@@ -107,15 +105,15 @@ class _ChangePasswordState extends State<ChangePassword> {
                     border: OutlineInputBorder(
                       borderSide: BorderSide(
                         color: isOldPasswordCorrect
-                            ? Colors.black // Use black when password is correct
-                            : Colors.red, // Use red for incorrect password
+                            ? Colors.black
+                            : Colors.red,
                       ),
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderSide: BorderSide(
                         color: isOldPasswordCorrect
                             ? Colors.orange
-                            : Colors.red, // Use red for incorrect password
+                            : Colors.red,
                       ),
                     ),
                     floatingLabelBehavior: FloatingLabelBehavior.always,
@@ -128,8 +126,8 @@ class _ChangePasswordState extends State<ChangePassword> {
                       onPressed: _togglePasswordVisibility1,
                       icon: Icon(
                         _passEnable1
-                            ? Icons.remove_red_eye_outlined
-                            : Icons.visibility_off_outlined,
+                            ? Icons.visibility_off_outlined
+                            : Icons.remove_red_eye_outlined,
                       ),
                       color: Colors.black87.withOpacity(0.6),
                     ),
@@ -143,7 +141,9 @@ class _ChangePasswordState extends State<ChangePassword> {
                     return null;
                   },
                 ),
+
                 SizedBox(height: 20),
+
                 TextFormField(
                   controller: _newPasswordController,
                   obscureText: _passEnable2,
@@ -164,8 +164,8 @@ class _ChangePasswordState extends State<ChangePassword> {
                       onPressed: _togglePasswordVisibility2,
                       icon: Icon(
                         _passEnable2
-                            ? Icons.remove_red_eye_outlined
-                            : Icons.visibility_off_outlined,
+                            ? Icons.visibility_off_outlined
+                            : Icons.remove_red_eye_outlined,
                       ),
                       color: Colors.black87.withOpacity(0.6),
                     ),
@@ -175,11 +175,15 @@ class _ChangePasswordState extends State<ChangePassword> {
                       return 'Please enter a new password.';
                     } else if (value.length < 8) {
                       return 'Password must be at least 8 characters long.';
+                    } else if (value == _oldPasswordController.text) {
+                      return 'New password cant be old password';
                     }
                     return null;
                   },
                 ),
+
                 SizedBox(height: 20),
+
                 TextFormField(
                   controller: _confirmPasswordController,
                   obscureText: _passEnable3,
@@ -200,8 +204,8 @@ class _ChangePasswordState extends State<ChangePassword> {
                       onPressed: _togglePasswordVisibility3,
                       icon: Icon(
                         _passEnable3
-                            ? Icons.remove_red_eye_outlined
-                            : Icons.visibility_off_outlined,
+                            ? Icons.visibility_off_outlined
+                            : Icons.remove_red_eye_outlined,
                       ),
                       color: Colors.black87.withOpacity(0.6),
                     ),
@@ -215,27 +219,39 @@ class _ChangePasswordState extends State<ChangePassword> {
                     return null;
                   },
                 ),
+
                 SizedBox(height: 20),
-                Center(
-                  child: SizedBox(
-                    height: 60,
-                    width: 500,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.orange,
-                        elevation: 2,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        padding: EdgeInsets.all(20),
+
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(22),
                       ),
-                      onPressed: () {
-                        Fluttertoast.showToast(msg: "Under construction");
-                      },
-                      child: Text(
-                        "Update",
-                        style: TextStyle(fontSize: 18, letterSpacing: 1),
-                      ),
+                      padding: EdgeInsets.all(20),
+                    ),
+                    onPressed: () async {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        await updatePassword(
+                          _email!,
+                          _oldPasswordController.text,
+                          _newPasswordController.text,
+                        );
+
+                        // password wrong hoile clear korar dorkar ase ?
+
+                        //_oldPasswordController.clear();
+                        //_newPasswordController.clear();
+                        //_confirmPasswordController.clear();
+                      }
+                    },
+                    child: Text(
+                      "Update",
+                      style: TextStyle(fontSize: 18, letterSpacing: 1),
                     ),
                   ),
                 ),
