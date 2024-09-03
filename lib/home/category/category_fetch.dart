0b11a4f05d179ecdb8bc246a1b0ecd1ff15bfeb6
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-09-03 12:44:40 +0600

    category

--- lib/home/category/category_fetch.dart
+++ lib/home/category/category_fetch.dart
@@ -3,23 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:learners/api/api_root.dart';
 
-
 class Category {
-  final String name;
+  final String category_id;
+  final String title;
   final String image;
-  final int courseCount;
+  final int description;
 
   Category({
-    required this.name,
+    required this.category_id,
+    required this.title,
     required this.image,
-    required this.courseCount,
+    required this.description,
   });
 
   factory Category.fromJson(Map<String, dynamic> json) {
     return Category(
-      name: json['name'],
-      image: json['image'],
-      courseCount: json['courseCount'],
+      category_id: json['category_id'] ?? 9999,
+      title: json['title'] ?? 'Unknown Name',
+      image: json['image'] ?? '',
+      description : json['description'] ?? "No description provided",
     );
   }
 }
@@ -30,16 +32,20 @@ class CategoryProvider with ChangeNotifier {
   List<Category> get categories => _categories;
 
   Future<void> fetchCategories() async {
-    //const String uri = "http://10.0.2.2/learners_api/login.php";
     const String uri = "${api_root}/categories.php";
 
-    final response = await http.get(Uri.parse(uri));
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      _categories = data.map((json) => Category.fromJson(json)).toList();
-      notifyListeners();
-    } else {
-      throw Exception('Failed to load categories');
+    try {
+      final response = await http.get(Uri.parse(uri));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+        _categories = data.map((json) => Category.fromJson(json)).toList();
+        notifyListeners();
+      } else {
+        throw Exception('Failed to load categories');
+      }
+    } catch (error) {
+      print('Error fetching categories: $error');
     }
   }
 }
